65a14d9849e553b093ad4e81670c15ea82fd99e6
0xGiddi <hello@giddi.net>
2017-03-18 00:41:17 +0200

    Added wimboot build script, fixed stage1/loader.ipxe paths, added stage2 dispart instructions

---
+++ utils/build_wimboot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# build_wimboot.sh
+# Build the wimboot module for stage0
+
+tmpDir="$(mktemp -d)"
+orgDir=$(pwd)
+
+sudo apt-get install -y pesign libiberty-dev binutils-dev
+git clone https://git.ipxe.org/wimboot.git "$tmpDir"
+
+
+cd "$tmpDir"/src
+make clean
+make wimboot
+cd "$orgDir" 
+cp "$tmpDir"/wimboot ../server/stage1/wimboot
