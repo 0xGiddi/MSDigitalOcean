88ababbfbab32664e7ea1acf1a3edee295d48a5e
0xGiddi <hello@giddi.net>
2017-03-19 01:31:48 +0200

    Fixed make_winpe,loader.cmd removed startne.cmd hack and added proper winpehl.ini

--- utils/make_winpe.sh
+++ utils/make_winpe.sh
@@ -24,19 +24,20 @@ winVersion="$(wimlib-imagex info "$dstDir"/boot.wim | grep ".Version" | awk 'NR%
 winArch="$(wimlib-imagex info "$dstDir"/boot.wim | grep "Architecture" | awk '{print $2}')"
 
 # Create a wim update File
+echo "Command file is: $cmdFile"
 cat > "$cmdFile" <<EOF
 rename /setup.exe /setup.exe.org
 rename /sources/setup.exe /sources/setup.exe.org
-add "$orgDir"/winpe_utils/wget.exe /windows/system32/wget.exe
-add "$orgDir"/winpe_utils/imagex.exe /windows/system32/imagex.exe
-add "$orgDir"/winpe_utils/drivers/$winVersion/$winArch/ /Drivers/
-add "$orgDir"/winpe_utils/startnet.cmd /windows/system32/starnet.cmd
-add "$orgDir"/winpe_utils/loader.cmd /windows/system32/loader.cmd
+add $orgDir/winpe_utils/wget.exe /windows/system32/wget.exe
+add $orgDir/winpe_utils/imagex.exe /windows/system32/imagex.exe
+add $orgDir/winpe_utils/drivers/$winVersion/$winArch/ /drivers/
+add $orgDir/winpe_utils/winpehl.ini /windows/system32/winpehl.ini
+add $orgDir/winpe_utils/loader.cmd /windows/system32/loader.cmd
 EOF
 
 # update the wim, but make sure that internal script files are all DOS formatted
 unix2dos "$orgDir"/winpe_utils/startnet.cmd
-unix2dos "$orgDir"/winpe_utils/loader.cmd
+unix2dos "$orgDir"/winpe_utils/winpehl.ini
 wimlib-imagex update "$dstDir"/boot.wim --check < "$cmdFile"
 
 cp "$dstDir"/boot.wim "$orgDir"/../server/stage1/boot/boot.wim
--- utils/winpe_utils/startnet.cmd
+++
@@ -1,2 +0,0 @@
-wpeinit
-loader
