624cd84b2ec2c1c15a540e0921501e0b2a62141e
0xGiddi <hello@giddi.net>
2017-03-20 22:38:04 +0200

    Commit for testing

--- utils/make_winpe.sh
+++ utils/make_winpe.sh
@@ -17,7 +17,10 @@ orgDir=$(pwd)
 
 # Export the wim image
 sudo mount $1 $tmpDir
-wimlib-imagex export "$tmpDir"/sources/boot.wim 2 --boot "$dstDir"/boot.wim 
+wimlib-imagex export "$tmpDir"/sources/boot.wim 2 --boot "$dstDir"/boot.wim
+
+# Get the case sensaive system32 path  (It's diffren between WinPE WIM files)
+sys32="$(dirname $(wimlib-imagex dir "$dstDir"/boot.wim  |  grep -Poie "^/windows/system32/cmd.exe$"))"
 
 # Find the arch and windows version for injecting drivers
 winVersion="$(wimlib-imagex info "$dstDir"/boot.wim | grep ".Version" | awk 'NR%2{printf "%s.",$3;next;}{print $3}')"
@@ -29,8 +32,8 @@ cat > "$cmdFile" <<EOF
 rename /setup.exe /setup.exe.org
 rename /sources/setup.exe /sources/setup.exe.org
 add $orgDir/winpe_utils/drivers/$winVersion/$winArch/ /drivers/
-add $orgDir/winpe_utils/winpeshl.ini /windows/system32/winpeshl.ini
-add $orgDir/winpe_utils/loadDrivers.cmd /windows/system32/loadDrivers.cmd
+add $orgDir/winpe_utils/winpeshl.ini $sys32/winpeshl.ini
+add $orgDir/winpe_utils/loadDrivers.cmd $sys32/loadDrivers.cmd
 EOF
 
 # update the wim, but make sure that internal script files are all DOS formatted
